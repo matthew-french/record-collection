95cd1543ba6fc97da28ff610ebdcb165ed5285af
= <matthew.french@gmail.com>
2024-07-02 10:57:19 +0100

    fix masonry lint errors

--- src/components/collections-masonry.tsx
+++ src/components/collections-masonry.tsx
@@ -31,47 +31,37 @@ const CollectionsMasonary: React.FC<DiscogResponse> = ({
   pagination,
   releases,
 }) => {
-  const [products, setProducts] = useState<Product[]>([])
   const currentPage = pagination.page || 1
 
-  const productsWithIndex = products.map((product, index) => ({
-    ...product,
-    index: index, // Adding an index property
-  }))
-
-  useEffect(() => {
-    // Convert releases to the desired format for products
-    const newProducts: Product[] = releases.map(
-      ({ basic_information }: DiscogRecord) => ({
-        coverImage: basic_information.cover_image,
-        id: basic_information.id,
-        thumb: basic_information.thumb,
-        year: basic_information.year,
-        title: basic_information.title,
-        genres: basic_information.genres.join(', '),
-        artist: {
-          name: basic_information.artists[0].name,
-          id: basic_information.artists[0].id,
-          resourceUrl: basic_information.artists[0].resource_url,
-        },
-        styles: basic_information.styles.join(', '),
-        masterUrl: basic_information.master_url,
-        resourceUrl: basic_information.resource_url,
-        formats: basic_information.formats
-          .map((format) => format.name)
-          .join(', '),
-      })
-    )
-
-    setProducts(newProducts) // Update the state with the new products
-  }, [currentPage, releases]) // Depend on currentPage and releases to trigger the effect
+  // Convert releases to the desired format for products
+  const products: Product[] = releases.map(
+    ({ basic_information }: DiscogRecord) => ({
+      coverImage: basic_information.cover_image,
+      id: basic_information.id,
+      thumb: basic_information.thumb,
+      year: basic_information.year,
+      title: basic_information.title,
+      genres: basic_information.genres.join(', '),
+      artist: {
+        name: basic_information.artists[0].name,
+        id: basic_information.artists[0].id,
+        resourceUrl: basic_information.artists[0].resource_url,
+      },
+      styles: basic_information.styles.join(', '),
+      masterUrl: basic_information.master_url,
+      resourceUrl: basic_information.resource_url,
+      formats: basic_information.formats
+        .map((format) => format.name)
+        .join(', '),
+    })
+  )
 
   return (
     <div className="masonry sm:masonry-sm md:masonry-md">
-      {productsWithIndex.map((product) => (
+      {products.map((product, index) => (
         <div className="rounded-lg p-1 pb-6 break-inside">
           <CollectionsRecordMasonry
-            key={`${product.id}-${product.index}`}
+            key={`${product.id}-${index}`}
             {...product}
           />
         </div>
